eb5500d88c54d967b106c19043b333724df17752
asif-sidd9768 <siddiqueasif421@gmail.com>
2023-01-19 13:12:22 +0530

    added axios

--- controllers/nextGadget.js
+++ controllers/nextGadget.js
@@ -1,11 +1,11 @@
 const nGRouter = require('express').Router()
+const axios = require('axios')
 
 nGRouter.get('/', async (req, res) => {
   const API_URL = `https://www.googleapis.com/youtube/v3/search?key=${process.env.API_KEY}&channelId=${process.env.CHANNEL_ID}&part=snippet,id&order=date&maxResults=4`;
   // Fetch latest videos from YouTube channel
-  const response = await fetch(API_URL)
-  const data = await response.json()
-  res.send("hello")
+  const response = await axios.get(API_URL)
+  res.send(response.data)
 })
 
 module.exports = nGRouter
