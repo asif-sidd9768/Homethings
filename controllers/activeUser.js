e2454db7c074753b822efecc06fb87f616cfae47
asif-sidd9768 <siddiqueasif421@gmail.com>
2023-10-19 18:32:12 +0530

    testing push notification scheduled

--- controllers/activeUser.js
+++ controllers/activeUser.js
@@ -2,20 +2,37 @@ const activeUserRouter = require('express').Router()
 const ActiveUser = require('../models/activeUser')
 const {Expo} = require('expo-server-sdk')
 
-activeUserRouter.get("/", async(req, res) => {
-  const activeUsers = await ActiveUser.find({}) 
-  
+const schedule = require('node-schedule');
+
+// Function to send birthday notifications
+const sendBirthdayNotifications = async () => {
+  const currentDate = new Date();
+
+  // Find active users whose birthday is today
+  const activeUsers = await ActiveUser.find({
+    birthday: {
+      $gte: currentDate,
+      $lt: new Date(currentDate.getTime() + 24 * 60 * 60 * 1000) // Check for birthdays in the next 24 hours
+    }
+  });
+
+  if (activeUsers.length === 0) {
+    console.log("No birthdays today");
+    return;
+  }
+
   let expo = new Expo({ accessToken: process.env.EXPO_ACCESS_TOKEN });
   let messages = [];
   for (let activeUser of activeUsers) {
-    // Construct a message (see https://docs.expo.io/push-notifications/sending-notifications/)
+    // Construct a message
     messages.push({
       to: activeUser.deviceToken,
       sound: 'default',
-      body: 'Someone\'s missing u',
-      data: { withSome: 'data' },
-    })
+      body: `Happy Birthday, ${activeUser.name}! 🎉`,
+      data: { birthdayNotification: true },
+    });
   }
+
   let chunks = expo.chunkPushNotifications(messages);
   let tickets = [];
   (async () => {
@@ -29,20 +46,74 @@ activeUserRouter.get("/", async(req, res) => {
       }
     }
   })();
+}
 
-  res.send(activeUsers)
-})
+// Schedule the job to run daily at a specific time (e.g., midnight)
+const job = schedule.scheduleJob('0 0 * * *', sendBirthdayNotifications);
 
-activeUserRouter.post("/", async(req, res) => {
-  const { name, username, deviceToken } = req.body
+activeUserRouter.get("/", async (req, res) => {
+  res.send("Server is ready to send birthday notifications.");
+});
+
+activeUserRouter.post("/", async (req, res) => {
+  const { name, username, deviceToken, birthday } = req.body;
+
   const activeUser = new ActiveUser({
     name,
     username,
-    deviceToken
-  })
-  console.log('active user == ', activeUser)
-  await activeUser.save()
-  res.send(activeUser)
-})
+    deviceToken,
+    birthday: new Date(birthday), // Convert birthday to a Date object
+  });
 
-module.exports = activeUserRouter
+  console.log('active user == ', activeUser);
+
+  await activeUser.save();
+  res.send(activeUser);
+});
+
+module.exports = activeUserRouter;
+
+// activeUserRouter.get("/", async(req, res) => {
+//   const activeUsers = await ActiveUser.find({}) 
+  
+//   let expo = new Expo({ accessToken: process.env.EXPO_ACCESS_TOKEN });
+//   let messages = [];
+//   for (let activeUser of activeUsers) {
+//     // Construct a message (see https://docs.expo.io/push-notifications/sending-notifications/)
+//     messages.push({
+//       to: activeUser.deviceToken,
+//       sound: 'default',
+//       body: 'Someone\'s missing u',
+//       data: { withSome: 'data' },
+//     })
+//   }
+//   let chunks = expo.chunkPushNotifications(messages);
+//   let tickets = [];
+//   (async () => {
+//     for (let chunk of chunks) {
+//       try {
+//         let ticketChunk = await expo.sendPushNotificationsAsync(chunk);
+//         console.log(ticketChunk);
+//         tickets.push(...ticketChunk);
+//       } catch (error) {
+//         console.error(error);
+//       }
+//     }
+//   })();
+
+//   res.send(activeUsers)
+// })
+
+// activeUserRouter.post("/", async(req, res) => {
+//   const { name, username, deviceToken } = req.body
+//   const activeUser = new ActiveUser({
+//     name,
+//     username,
+//     deviceToken
+//   })
+//   console.log('active user == ', activeUser)
+//   await activeUser.save()
+//   res.send(activeUser)
+// })
+
+// module.exports = activeUserRouter
